46472b6b69cb609dfd1f9ce3a0391a3bebc1df3f
SDaubert@evident.io <sdaubert@evident.io>
2018-10-27 20:48:33 -0700

    integrate credo, dialyzer, dockerhub, circleci

--- mix.exs
+++ mix.exs
@@ -6,7 +6,8 @@ defmodule GameServices.Umbrella.MixProject do
       apps_path: "apps",
       version: "0.1.0",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [plt_add_apps: [:mix]]
     ]
   end
 
