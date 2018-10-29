1f99ae3d621f64c0c18db01943ea266c78597ba6
Alezrik <sdaubert@evident.io>
2018-10-28 20:25:39 -0700

    workflows

--- apps/game_services_web/lib/game_services_web/controllers/login_controller.ex
+++ apps/game_services_web/lib/game_services_web/controllers/login_controller.ex
@@ -2,11 +2,20 @@ defmodule GameServicesWeb.LoginController do
   @moduledoc false
   use GameServicesWeb, :controller
 
+  require Logger
   def index(conn, _params) do
     render(conn, "index.html")
   end
 
-  def login(conn, _params) do
-    render(conn, "index.html")
+  def login(conn, %{"login" => login}) do
+    case Authentication.get_user_by_credential(Map.get(login, "username"), Map.get(login, "password")) do
+      {:ok, user} ->
+        Logger.debug("login success: #{inspect user}")
+        redirect(conn, to: Routes.page_path(conn, :index))
+       {error, reason} ->
+         Logger.debug "login fail: #{inspect reason}"
+         render(conn, "index.html")
+    end
+
   end
 end
