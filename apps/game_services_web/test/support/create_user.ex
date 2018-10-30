845e030ffea42ea54e2644355dbb64e8bcdc2e40
Alezrik <sdaubert@evident.io>
2018-10-30 11:13:30 -0700

    logout route https://github.com/Alezrik/game_services_umbrella/issues/21

--- apps/game_services_web/test/support/create_user.ex
+++ apps/game_services_web/test/support/create_user.ex
@@ -1,4 +1,8 @@
 defmodule GameServicesWeb.CreateUser do
+  @moduledoc false
+
+
+  ## helper to create user in test with checking out repo
   def get_user() do
     Ecto.Adapters.SQL.Sandbox.mode(GameServices.Repo, {:shared, self()})
     {:ok, user} = GameServices.Account.create_user()
