bec5d730f21e3779fc4f6dec40bb37d7d45b86ec
Alezrik <ex_ra@hotmail.com>
2018-12-02 23:21:41 -0800

    resize the salt len to 32bit

--- apps/tcp_server/lib/tcp_server/command_serializer.ex
+++ apps/tcp_server/lib/tcp_server/command_serializer.ex
@@ -19,7 +19,7 @@ defmodule TcpServer.CommandSerializer do
         #        Logger.error(fn -> "pw_salt: #{pw_salt}" end)
         #        Logger.error(fn -> "server rnd: #{server_rnd}" end)
         pw_salt_len = byte_size(pw_salt)
-        message = <<server_rnd::size(32), pw_salt_len::size(8)>> <> pw_salt
+        message = <<server_rnd::size(32), pw_salt_len::size(32)>> <> pw_salt
         message_len = byte_size(message)
 
         Logger.info(fn -> "Serialize SMSG_AUTHENTICATE_CHALLENGE to binary" end,
